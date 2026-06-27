3e57feb2d0b78043f65f4908c7f5ff9ee796b69a
Ayman-dev1 <aymanelwan67@gmail.com>
2026-06-28 00:14:11 +0300

    deployment setup

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ initSecurity(app, server);
 
 // Middleware
 app.use(cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+    origin: process.env.FRONTEND_URL,
     credentials: true,
 }));
 app.use(express.json({ limit: '50mb' }));
